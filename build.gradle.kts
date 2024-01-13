9b6ff5a4bf91e34d385a690c56ac87eccf7d8439
Matheus C. O. Santos <42250854+matheuscosantos@users.noreply.github.com>
2024-01-12 21:08:10 -0300

    Atualiza projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.1"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.22"
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.21"
-	kotlin("plugin.jpa") version "1.8.22"
+	kotlin("plugin.jpa") version "1.9.21"
 }
 
 group = "br.com.fiap.mikes"
