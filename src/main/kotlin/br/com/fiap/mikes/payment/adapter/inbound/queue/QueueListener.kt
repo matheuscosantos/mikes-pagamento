08e0ac37bf31f1e606444e5da70a1d82976ab4d5
Matheus C. O. Santos <42250854+matheuscosantos@users.noreply.github.com>
2024-01-16 00:16:59 -0300

    altera listener

--- src/main/kotlin/br/com/fiap/mikes/payment/adapter/inbound/queue/QueueListener.kt
+++ src/main/kotlin/br/com/fiap/mikes/payment/adapter/inbound/queue/QueueListener.kt
@@ -3,8 +3,10 @@ package br.com.fiap.mikes.payment.adapter.inbound.queue
 import br.com.fiap.mikes.payment.adapter.inbound.queue.dto.QueueMessageDTO
 import br.com.fiap.mikes.payment.adapter.outbound.messenger.sns.client.DefaultSnsMessenger
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.springframework.stereotype.Component
 import java.util.logging.Logger
 
+@Component
 class QueueListener(
     val topicClient: DefaultSnsMessenger,
 ) {
