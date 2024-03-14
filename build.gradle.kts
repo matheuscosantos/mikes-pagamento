70a3c02ab18055122f46c7217e533c653b7d66e0
wagnerlm <wagnerlink@gmail.com>
2024-03-14 19:14:52 -0300

    configure security

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("io.awspring.cloud:spring-cloud-aws-starter-sns:3.1.0")
     implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs:3.1.0")
